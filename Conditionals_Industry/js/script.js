bfc05c59f68777621acf795f320745565ed25e74
Andre Guerra <AndreGuerra@Andres-MacBook-Pro.local>
2014-02-20 14:49:25 -0800

    commit

---
+++ Conditionals_Industry/js/script.js
@@ -0,0 +1,6 @@
+//Guerra, Andre 02/20/14 Conditionals - Industry
+
+var blogs = prompt("How many blogs did you publish?");
+var leadsGenerated = prompt("How many leads did you recieve from your blogs this month?");
+var leadGoal = prompt("What is yoru desired lead goal?")
+
