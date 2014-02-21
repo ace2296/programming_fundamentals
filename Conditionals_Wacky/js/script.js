c28c045cf549a4b9dcb72eafc8b1b68f8818a91f
Andre Guerra <AndreGuerra@Andres-MacBook-Pro.local>
2014-02-20 21:16:13 -0800

    Entered if empty values

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -5,10 +5,10 @@ var employees = prompt("How many employees will need computers?"); //This will b
 
 var perEmpBudget = budget / employees; //Get the total amount you can spend per employee
 
-if (budget == ""){
-	var budget = prompt("Oops! It looks like you forgot to set a budget. Please enter one now:");
-	} else if (employees == ""){
-		var employees = prompt("Oops! It looks like you forgot to set the number of employees need computers. Please enter this now:");
+if (budget == ""){ //Check if budget is empty
+	var budget = prompt("Oops! It looks like you forgot to set a budget. Please enter one now:"); //Prompt the user to input a budget value
+	} else if (employees == ""){ //Check if employees is empty
+		var employees = prompt("Oops! It looks like you forgot to set the number of employees need computers. Please enter this now:"); //Prompt the user to input a employees value
 	} else if (perEmpBudget >= 900 ) { //If per employee budget is greater than $900
 	console.log("You are able to buy everyone a MacBook Air!"); //Outcome for budget $900
 	} else if (perEmpBudget >= 1500) { //Outcome for budget $1500
