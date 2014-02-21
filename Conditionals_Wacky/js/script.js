bf65c7a76c98b100c48ea7559bf018b00d34f6d3
Andre Guerra <AndreGuerra@Andres-MacBook-Pro.local>
2014-02-20 21:02:44 -0800

    wacky

---
+++ Conditionals_Wacky/js/script.js
@@ -0,0 +1,20 @@
+//Guerra, Andre 02/20/14 Conditionals - Wacky
+
+var budget = prompt("What is your total computer budget?"); //Sets the total computer budget
+var employees = prompt("How many employees will need computers?"); //This will be the number the budget is divided into
+
+var perEmpBudget = budget / employees; //Get the total amount you can spend per employee
+
+if (budget == ""){
+	var budget = prompt("Oops! It looks like you forgot to set a budget. Please enter one now:");
+	} else if (employees == ""){
+		var employees = prompt("Oops! It looks like you forgot to set the number of employees need computers. Please enter this now:");
+	} else if (perEmpBudget >= 900 ) { //If per employee budget is greater than $900
+	console.log("You are able to buy everyone a MacBook Air!"); //Outcome for budget $900
+	} else if (perEmpBudget >= 1500) { //Outcome for budget $1500
+		console.log("You are able to buy everyone a MacBook Pro!"); // Result of a Budget between 1500 and 2000
+	} else if (perEmpBudget >= 2000) { //Outcome for budget $2,000
+		console.log("You are able to buy everyone a Mac Tower!"); // Result of a Budget of $2000 +
+	} else {
+	console.log("It looks like your buying everyone a PC.") ; // Result of a Budget below $900
+}
