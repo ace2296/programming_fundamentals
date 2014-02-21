a19494566e7cb321a79735159f39473905f94358
Andre Guerra <AndreGuerra@Andres-MacBook-Pro.local>
2014-02-20 19:53:53 -0800

    inserted vars

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -1,6 +1,15 @@
 //Guerra, Andre 02/20/14 Conditionals - Industry
 
+var leadGoal = prompt("What is your desired lead goal?");
+var visits = prompt("How many website visitors did you get this month?");
 var blogs = prompt("How many blogs did you publish?");
 var leadsGenerated = prompt("How many leads did you recieve from your blogs this month?");
-var leadGoal = prompt("What is yoru desired lead goal?")
+var leadActual = (visits / blogs) * conversionPercent;
+var conversionPercent = 0.10;
+var remainingLeads = leadGoal % leadsGenerated; 
 
+if (leadsGenerated >= leadGoal ) {
+	console.log("Congratulations! You've hit your lead goal with a total of" + leadsGenerated + " leads.")
+} else {
+	console.log("It looks like your " + remainingLeads + " leads away from hitting your lead goal.") ;
+}
