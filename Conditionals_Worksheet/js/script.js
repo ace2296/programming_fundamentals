4089871cc3308a2a877c688e81243e0384601b5b
Andre Guerra <AndreGuerra@Andres-MacBook-Pro.local>
2014-02-20 22:47:41 -0800

    Inserted temp notes

--- Conditionals_Worksheet/js/script.js
+++ Conditionals_Worksheet/js/script.js
@@ -1,23 +1,23 @@
 //Guerra, Andre 02/20/14 Conditionals - Worksheet
 
-var temp = prompt("Enter a temperature to convert:");
-var unit = prompt("Enter F or C for starting temp unit:");
+var temp = prompt("Enter a temperature to convert:"); //Beginning temp to convert
+var unit = prompt("Enter F or C for starting temp unit:"); //Starting unit to conver from
 
-var cTemp = (temp - 32) * 5 / 9;
-var fTemp = temp * 9 / 5 + 32;
+var cTemp = (temp - 32) * 5 / 9; //Formula to convert celsius to fahrenheight
+var fTemp = temp * 9 / 5 + 32;//Formula to convert fahrenheight to celsius
 
-if (temp === "") {
+if (temp === "") { //Check to see if temp var is empty
 	prompt("Oops! You forgot to enter a temperature. Please enter one now without the unit:");
 }
 
-if (unit === "") {
+if (unit === "") { //Check to see if unit var is empty
 	prompt("Oops! You forgot to enter a unit. Please enter F or C:");
 }
 
-if (unit === "C") {
+if (unit === "C") { //Convert C to F
 	console.log("The temparature you entered converts into " + fTemp + " degrees fahrenheit.");
 }
 
-if (unit === "F") {
+if (unit === "F") { //Convert F to C
 	console.log("The temparature you entered converts into " + cTemp + " degrees celsius.");
 }
