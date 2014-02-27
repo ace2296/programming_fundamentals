199f452e9cb5161ce9df939ec4d034ef25523a78
Andre Guerra <AndreGuerra@Andres-MacBook-Pro.local>
2014-02-27 07:22:22 -0800

    Created pixel to em converter

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -1,5 +1,12 @@
 //Guerra, Andre 02/27/14 Functions - Industry
 
-function averageCommits() {
-	
+//px to em converter
+
+function converter(px) { //Set function for user to enter a pixel value
+	var em = 0.063; //Sets the amount of ems per pixel
+	var result = px * em;
+	console.log (px + "px converts into " + result + "em.");
 }
+
+converter(prompt('what is the pixel amount you want to convert?'));
+
