44d7c5d23db9d6f51e9ea44009e98ac3f8181bae
Andre Guerra <AndreGuerra@Andres-MacBook-Pro.local>
2014-02-27 07:23:35 -0800

    inserted additional notes

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -4,9 +4,9 @@
 
 function converter(px) { //Set function for user to enter a pixel value
 	var em = 0.063; //Sets the amount of ems per pixel
-	var result = px * em;
-	console.log (px + "px converts into " + result + "em.");
+	var result = px * em; //Get the result of ems per pixel
+	console.log (px + "px converts into " + result + "em."); //Output result
 }
 
-converter(prompt('what is the pixel amount you want to convert?'));
+converter(prompt('what is the pixel amount you want to convert?')); //Call-out function and ask for the beginningpixel amount to convert
 
