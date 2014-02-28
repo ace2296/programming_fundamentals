b270a989c6b9b38c4a2fdd3f61aff44a2ab78eea
Andre Guerra <AndreGuerra@Andres-MacBook-Pro.local>
2014-02-27 21:37:43 -0800

    Entered kegs to beer equation

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -2,9 +2,16 @@
 
 //Find out how much beer the college kids will drink
 
-const pi = 3.14;
+var amount = prompt("How many kegs will be at the party?");
 
-function calVolume(height,radius,amount) {
-	var volume = (height * pi * radius)^2;
+function calVolume(height,pi,radius) {
+	var volume = height * pi * radius^2;
 	return volume;
 }
+
+var beers = amount * 58.67 / 11.5;
+beers = Math.ceil(beers * 10) / 10; //This isn't something we learned yet, but this snippet will round the number of beers up to the nearest 10th
+
+var liters = calVolume(20,12,3.14);
+
+console.log("The college party animals will drink a total of " + liters * amount + " liters of beer at the party. Which comes out to to a total of " + beers + " beers");
